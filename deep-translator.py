a01bbb39ccd35b74ab9a725726c247a52c608a71
adryanmaikel <adryanmaikel@gmail.com>
2023-11-16 23:47:56 -0300

    traduzindo um texto com deep_translator

---
+++ deep-translator.py
@@ -0,0 +1,11 @@
+from deep_translator import GoogleTranslator
+
+PT, EN = 'pt', 'en'
+
+GOOGLE = GoogleTranslator(source=PT, target=EN)
+
+NAME, AGE = 'Adryan Maikel da Cunha Kuhne', 21
+TEXT = f'Olá Mundo! Meu nome é {NAME}, e tenho {AGE} anos de idade.'
+
+translator = GOOGLE.translate(TEXT)
+print(translator)
