23502c91bf708887902eccc60fafd20743cf08ac
AdryanMaikel <adryanmaikel@gmail.com>
2024-02-04 22:11:33 -0300

    criando função para ocultar o navegador e adicionando docstrings

--- _Seleniums/main.py
+++ _Seleniums/main.py
@@ -1,4 +1,7 @@
 """
+    Utilizarei o selenium para executar uma automação simples onde irá
+entrar no site do google e pesquisar por bitcoin, encontrar o elemento
+contendo o preço e retorna-lo em formato de string.
 """
 
 from time import sleep
@@ -8,17 +11,22 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-show_browser = True
 OPTIONS = Options()
-if not show_browser:
-    OPTIONS.add_argument("--headless")
 
 
 def test_firefox() -> bool:
     """Função para testar se o firefox está funcionando.
     """
-    browser = Firefox(options=OPTIONS)
+    browser = Firefox()
+    input("Precione enter para fechar o navegador.")
     browser.close()
+
+
+def hide_browser() -> None:
+    """Função utilizada para ocultar a janela do navegador, executando
+    as ações em segundo plano.
+    """
+    OPTIONS.add_argument("--headless")
 
 
 def search_google(words: str, browser: Firefox) -> None:
@@ -27,18 +35,32 @@ def search_google(words: str, browser: Firefox) -> None:
 
     Args:
         words (str): Palavras a serem pesquisadas no google.
-        browser (Firefox): webdriver do selenium navegador Firefox.
+        browser (Firefox): Webdriver do selenium, navegador Firefox.
     """
     SITE_GOOGLE = "https://www.google.com/"
-    XPATHS = {"search bar": "//*[@id='APjFqb']"}
+    XPATHS_SEARCH_BAR = "//*[@id='APjFqb']"
     browser.get(SITE_GOOGLE)
-    sourch_bar = browser.find_element(By.XPATH, XPATHS["search bar"])
+    sourch_bar = browser.find_element(By.XPATH, XPATHS_SEARCH_BAR)
     sourch_bar.click()
     sourch_bar.send_keys(words)
     sourch_bar.submit()
 
 
 def get_price_bitcoin(browser: Firefox) -> str:
+    """Função para pegar o preço do bitcoin na página de resultado da
+    pesquisa do google com base no XPATH do elemento, fica pesquisando o
+    elemento até encontrar a cada 1 segundo.
+
+    Args:
+        browser (Firefox): Webdriver do selenium, navegador Firefox.
+
+    Returns:
+        str: Preço do bitcoin.
+
+    >>> bitcoin_price = get_price_bitcoin()
+    >>> print(bitcoin_price)
+    ... 211.464,46
+    """
     XPATH_PRICE = "/html/body/div[5]/div/div[12]/div[1]/div[2]/div[2]/div/div"\
                   "/div[1]/div/block-component/div/div[1]/div/div/div/div[1]/"\
                   "div/div/div/div/div/div/div/div/div[3]/div[2]/span[1]"
@@ -52,13 +74,20 @@ def get_price_bitcoin(browser: Firefox) -> str:
 
 
 def search_bitcoin_price_on_google() -> str:
+    """Função que pesquisa o preço do bitcoin no google, e retorna o
+    preço em formato de string.
+
+    Returns:
+        str: Preço do Bitcoin em tempo real.
+    """
     browser = Firefox(options=OPTIONS)
     search_google(words="bitcoin", browser=browser)
     bitcoin_price = get_price_bitcoin(browser=browser)
-    print(bitcoin_price)
-    input()
     browser.close()
+    return bitcoin_price
 
 
 if __name__ == "__main__":
-    search_bitcoin_price_on_google()
+    hide_browser()
+    bitcoin_price = search_bitcoin_price_on_google()
+    print(bitcoin_price)
